d45878145811f53a3be21f01e3c293432e131f4d
Samuuu <cristiangarciapineda@gmail.com>
2026-09-22 07:31:44 -0500

    Primer commit

---
+++ src/Ejercicio01.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+
+public class Ejercicio01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] consumos = new int {1, 2, 3, 4, 5, 6, 7, 8, 9, 10} {
+
+
+            for (int i = 0; i < args.length; i++) {
+                
+                if 
+
+                }
+            }
+
+            
+        }
+
+
+
+        scanner.close();
+    }
+}
